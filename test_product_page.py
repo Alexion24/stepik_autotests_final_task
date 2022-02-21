fe9e95e5ea1e06045e576dc0982df1aa4ce9346e
Alexion <Alex24Bryant@mail.ru>
2022-02-22 03:26:30 +0500

    added changes from Module 4.3 step 4

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.mark.parametrize('promo_offer', ["0", "1", "3", "4", "5", "6", "7", "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer[0]}"
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
