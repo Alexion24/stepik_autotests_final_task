8d09fcf363e9bd25c8dc4e54869c4a43a358a54b
Alexion <Alex24Bryant@mail.ru>
2022-02-21 20:02:43 +0500

    added changes from step 10

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,6 +7,8 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to_alert
+        alert.accept()
 
     def should_be_login_link(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
