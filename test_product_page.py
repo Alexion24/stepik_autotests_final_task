f22fe97a23f030789a188b841b96571629816132
Alexion <Alex24Bryant@mail.ru>
2022-02-22 16:35:27 +0500

    added changes from Module 4.3 step 8

--- test_product_page.py
+++ test_product_page.py
@@ -28,7 +28,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_not_be_success_message()
 
 
-# @pytest.mark.smoke
+# @pytest.mark.new
 def test_guest_cant_see_success_message(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
     page = ProductPage(browser, link)
@@ -46,7 +46,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_disapear_of_success_message()
 
 
-@pytest.mark.smoke
+# @pytest.mark.new
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -54,7 +54,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-@pytest.mark.smoke
+# @pytest.mark.new
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
